415c4244f951c495d9af6a74d7c1b3e1ba43971b
leul <leulsegedgebremedhin@gmail.com>
2024-03-10 02:36:23 +0300

    commit

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+export const supabaseUrl = "https://dumhitkuecajzeddtgin.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1bWhpdGt1ZWNhanplZGR0Z2luIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5ODgwNjEsImV4cCI6MjAyNTU2NDA2MX0.qKZOh5SKGlTNdiQGFXehcD-mHEtVjwovYEzZAzUdOTo";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
